0779b2ae6dadb04bbb06b28a54a84ddff33e0254
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-25 17:02:49 -0800

    DbContext set up with composite keys and DbSets

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using AsyncInn.Models;
 
 namespace AsyncInn.Data
 {
@@ -12,5 +13,19 @@ namespace AsyncInn.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //need to add composite key associations here for HotelRoom and RoomAmenities
+            modelBuilder.Entity<HotelRoom>().HasKey(hotelRoom => new { hotelRoom.HotelId, hotelRoom.RoomNumber });
+            modelBuilder.Entity<RoomAmenities>().HasKey(amen => new { amen.AmenitiesID, amen.RoomID });
+        }
+
+        //setup DbSets
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+        public DbSet<Amenities> Amenities { get; set; }
     }
 }

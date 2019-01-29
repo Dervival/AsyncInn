278dc8859c59722bcc81d93711433a5b3698593b
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-29 11:27:00 -0800

    Added interfaces for Hotel, Amenities, Room models

---
+++ AsyncInn/AsyncInn/Models/Interfaces/IRoomConfigManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    interface IRoomConfigManager
+    {
+        //Create a room configuration
+        Task CreateConfiguration(Room room);
+        //Read in a configuration
+        Task<Room> GetConfiguration(int id);
+        //Read in several room configurations
+        Task<IEnumerable<Room>> GetConfigurations();
+        //Update a room
+        void UpdateRoom(Room room);
+        //Delete a room
+        void DeleteRoom(int id);
+    }
+}
